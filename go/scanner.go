8c08961f4581618cd114ff440a2104449903a99e
Jan Mercl <jnml@fsc-r550.(none)>
2013-08-26 11:10:11 +0200

    go: make .Error "clean" again

--- go/scanner.go
+++ go/scanner.go
@@ -11,6 +11,7 @@
 package scanner
 
 import (
+	"errors"
 	"fmt"
 	"go/token"
 	"strconv"
@@ -81,7 +82,7 @@ func (s *Scanner) err(format string, arg ...interface{}) {
 
 // Error implements yyLexer.
 func (s *Scanner) Error(msg string) {
-	s.err("%s", msg)
+	s.Errors = append(s.Errors, errors.New(msg))
 }
 
 // Scan scans the next token and returns the token and its value if applicable.
